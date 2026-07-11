b2380a60fa3a60b53923623a329e7168ea461dcb
Barak Bloch <barak.bloch@gmail.com>
2026-07-11 22:27:04 +0300

    Backport v2 menu system + .harmony project files (rev 10)
    - New floating glass toolbar: single colour swatch, brush selector,
      brush-size slider, save, clear; overflow (...) menu with theme,
      background colour and .harmony download/upload; about moved to the bar.
    - Light/dark theme (auto/light/dark), persisted; design-token styling.
    - Reusable glass modal (keyboard-operable, defaults to the safe choice)
      replacing native confirm() for Clear and the import overwrite warning.
    - .harmony project format (zip via vendored fflate): image.png +
      map.json (brush point map) + manifest.json (date, selected_brush,
      canvas dims). Upload validates the PNG header (anti-bomb), the
      image<->manifest size coordination, and the point map (count/bounds).
    - Brush-size slider kept in sync with the d/f shortcuts.
    
    Co-authored-by: @barakbl

--- js/about.js
+++ js/about.js
@@ -1,64 +1,87 @@
 function About()
 {
-	this.init();	
+	this.init();
 }
 
-About.prototype = 
+About.prototype =
 {
 	container: null,
 
 	init: function()
 	{
-		var text, containerText;
-		
+		var scope = this, text, closeButton;
+
 		this.container = document.createElement("div");
-		this.container.className = 'gui';
+		this.container.className = 'about-panel';
 		this.container.style.position = 'absolute';
 		this.container.style.top = '0px';
 		this.container.style.visibility = 'hidden';
-		
-		containerText = document.createElement("div");
-		containerText.style.margin = '10px 10px';
-		containerText.style.textAlign = 'left';
-		this.container.appendChild(containerText);
+
+		text = document.createElement("h1");
+		text.innerHTML = 'Harmony';
+		this.container.appendChild(text);
 
 		text = document.createElement("p");
-		text.style.textAlign = 'center';		
-		text.innerHTML = '<strong>HARMONY</strong> <a href="changelog.txt" target="_blank">r' + REV + '</a> by <a href="http://twitter.com/mrdoob" target="_blank">Mr.doob</a>';
-		containerText.appendChild(text);
+		text.className = 'sub';
+		text.innerHTML = '<a href="changelog.txt" target="_blank">r' + REV + '</a> by <a href="http://twitter.com/mrdoob" target="_blank">Mr.doob</a>';
+		this.container.appendChild(text);
 
-		text = document.createElement("p");
-		text.style.textAlign = 'center';
-		text.innerHTML = 'Brush: <span class="key">d</span><span class="key">f</span> size, <span class="key">r</span> reset<br />Color: <span class="key">shift</span> wheel, <span class="key">alt</span> picker<br />';
-		containerText.appendChild(text);
+		text = document.createElement("h3");
+		text.innerHTML = 'Shortcuts';
+		this.container.appendChild(text);
 
-		text = document.createElement("p");
-		text.style.textAlign = 'center';
-		text.innerHTML = '<a href="http://mrdoob.com/blog/post/689" target="_blank">Info</a> - <a href="http://github.com/mrdoob/harmony" target="_blank">Source Code</a>';
-		containerText.appendChild(text);
+		text = document.createElement("div");
+		text.className = 'kbd-row';
+		text.innerHTML =
+			'<span><span class="key">d</span> <span class="key">f</span> brush size</span>' +
+			'<span><span class="key">r</span> reset brush</span>' +
+			'<span><span class="key">shift</span> colour wheel</span>' +
+			'<span><span class="key">alt</span> eyedropper</span>';
+		this.container.appendChild(text);
 
 		text = document.createElement("hr");
-		containerText.appendChild(text);
+		this.container.appendChild(text);
 
 		text = document.createElement("p");
-		text.innerHTML = '<em>Sketchy</em>, <em>Shaded</em>, <em>Chrome</em>, <em>Fur</em>, <em>LongFur</em> and <em>Web</em> are all variations of the neighbour points connection concept. First implemented in <a href="http://www.zefrank.com/scribbler/" target="_blank">The Scribbler</a>.';
-		containerText.appendChild(text);
-		
-		text = document.createElement("p");
-		text.innerHTML = 'If you like the tool, you can use this button to share your love ;)';
-		containerText.appendChild(text);
-		
+		text.innerHTML = '<em>Sketchy</em>, <em>Shaded</em>, <em>Chrome</em>, <em>Fur</em>, <em>LongFur</em> and <em>Web</em> are all variations of the neighbour points connection concept, first implemented in <a href="http://www.zefrank.com/scribbler/" target="_blank">The Scribbler</a>.';
+		this.container.appendChild(text);
+
 		text = document.createElement("p");
 		text.style.textAlign = 'center';
-		text.innerHTML = '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="VY7767JMMMYM4"><input type="image" src="https://www.paypal.com/en_GB/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online."><img alt="" border="0" src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1"></form>';
-		containerText.appendChild(text);
+		text.innerHTML = '<a href="http://mrdoob.com/blog/post/689" target="_blank">Info</a> &nbsp;·&nbsp; <a href="http://github.com/mrdoob/harmony" target="_blank">Source Code</a>';
+		this.container.appendChild(text);
+
+		text = document.createElement("hr");
+		this.container.appendChild(text);
+
+		text = document.createElement("p");
+		text.style.textAlign = 'center';
+		text.innerHTML = 'If you like the tool, you can use this button to share your love ;)';
+		this.container.appendChild(text);
+
+		text = document.createElement("p");
+		text.style.textAlign = 'center';
+		text.style.margin = '10px 0 0';
+		text.innerHTML = '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank" style="margin:0"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="VY7767JMMMYM4"><input type="image" src="https://www.paypal.com/en_GB/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online."><img alt="" border="0" src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1"></form>';
+		this.container.appendChild(text);
+
+		closeButton = document.createElement("button");
+		closeButton.type = 'button';
+		closeButton.className = 'about-close';
+		closeButton.innerHTML = 'Close';
+		closeButton.addEventListener('click', function()
+		{
+			scope.hide();
+			isAboutVisible = false;
+		}, false);
+		this.container.appendChild(closeButton);
 	},
-	
+
 	show: function()
 	{
-		this.container.style.visibility = 'visible';		
+		this.container.style.visibility = 'visible';
 	},
-	
+
 	hide: function()
 	{
 		this.container.style.visibility = 'hidden';
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const REV = 9;
+const REV = 10;
 const BRUSHES = ["sketchy", "shaded", "chrome", "fur", "longfur", "web", "", "simple", "squares", "ribbon", "", "circles", "grid"];
 const USER_AGENT = navigator.userAgent.toLowerCase();
 
@@ -28,6 +28,8 @@ var SCREEN_WIDTH = window.innerWidth,
     isBgColorSelectorVisible = false,
     isAboutVisible = false,
     isMenuMouseOver = false,
+    isCanvasDirty = false,
+    isModalOpen = false,
     shiftKeyIsDown = false,
     altKeyIsDown = false;
 
@@ -36,6 +38,8 @@ init();
 function init()
 {
 	var hash, palette, embed, localStorageImage;
+
+	themeApply( themeGet() );
 
 	if (USER_AGENT.search("android") > -1 || USER_AGENT.search("iphone") > -1)
 		BRUSH_SIZE = 2;
@@ -90,17 +94,25 @@ function init()
 	menu = new Menu();
 	menu.foregroundColor.addEventListener('click', onMenuForegroundColor, false);
 	menu.foregroundColor.addEventListener('touchend', onMenuForegroundColor, { passive: false });
-	menu.backgroundColor.addEventListener('click', onMenuBackgroundColor, false);
-	menu.backgroundColor.addEventListener('touchend', onMenuBackgroundColor, false);
+	menu.background.addEventListener('click', onMenuBackgroundColor, false);
+	menu.background.addEventListener('touchend', onMenuBackgroundColor, false);
 	menu.selector.addEventListener('change', onMenuSelectorChange, false);
+	menu.sizeSlider.addEventListener('input', onMenuSizeChange, false);
 	menu.save.addEventListener('click', onMenuSave, false);
 	menu.save.addEventListener('touchend', onMenuSave, false);
 	menu.clear.addEventListener('click', onMenuClear, false);
 	menu.clear.addEventListener('touchend', onMenuClear, false);
 	menu.about.addEventListener('click', onMenuAbout, false);
 	menu.about.addEventListener('touchend', onMenuAbout, false);
+	menu.download.addEventListener('click', onMenuDownload, false);
+	menu.download.addEventListener('touchend', onMenuDownload, false);
+	menu.upload.addEventListener('click', onMenuUpload, false);
+	menu.upload.addEventListener('touchend', onMenuUpload, false);
+	menu.fileInput.addEventListener('change', onUploadFileChange, false);
 	menu.container.addEventListener('mouseover', onMenuMouseOver, { passive: false });
 	menu.container.addEventListener('mouseout', onMenuMouseOut, { passive: false });
+	menu.overflowPanel.addEventListener('mouseover', onMenuMouseOver, { passive: false });
+	menu.overflowPanel.addEventListener('mouseout', onMenuMouseOut, { passive: false });
 	container.appendChild(menu.container);
 
 	if (STORAGE)
@@ -115,6 +127,7 @@ function init()
 				context.restore();
 				context.drawImage(localStorageImage,0,0);
 				context.scale(PIXEL_RATIO, PIXEL_RATIO);
+				isCanvasDirty = true;
 			}, false);
 
 			localStorageImage.src = localStorage['harmony-canvas'];
@@ -206,6 +219,9 @@ function onWindowResize()
 
 function onWindowKeyDown( event )
 {
+	if (isModalOpen)
+		return;
+
 	if (shiftKeyIsDown)
 		return;
 
@@ -224,16 +240,21 @@ function onWindowKeyDown( event )
 
 		case 68: // d
 			if(BRUSH_SIZE > 1) BRUSH_SIZE --;
+			menu.setBrushSize( BRUSH_SIZE );
 			break;
 
 		case 70: // f
 			BRUSH_SIZE ++;
+			menu.setBrushSize( BRUSH_SIZE );
 			break;
 	}
 }
 
 function onWindowKeyUp( event )
 {
+	if (isModalOpen)
+		return;
+
 	switch(event.keyCode)
 	{
 		case 16: // Shift
@@ -362,6 +383,13 @@ function onMenuBackgroundColor()
 	isBgColorSelectorVisible = true;
 }
 
+function onMenuSizeChange()
+{
+	BRUSH_SIZE = parseInt(menu.sizeSlider.value);
+	menu.setBrushSize( BRUSH_SIZE );
+	context.lineCap = BRUSH_SIZE == 1 ? 'butt' : 'round';
+}
+
 function onMenuSelectorChange()
 {
 	if (BRUSHES[menu.selector.selectedIndex] == "")
@@ -402,15 +430,341 @@ function onMenuSave()
 
 function onMenuClear()
 {
-	if (!confirm("Are you sure?"))
+	openModal({
+		danger: true,
+		icon: ICON.trash,
+		title: 'Clear canvas?',
+		message: "This will erase your current drawing. This can't be undone.",
+		confirmLabel: 'Clear',
+		cancelLabel: 'Cancel',
+		onConfirm: function()
+		{
+			context.clearRect(0, 0, SCREEN_WIDTH * PIXEL_RATIO, SCREEN_HEIGHT * PIXEL_RATIO);
+
+			isCanvasDirty = false;
+
+			if (STORAGE) saveToLocalStorage();
+
+			brush.destroy();
+			brush = eval("new " + BRUSHES[menu.selector.selectedIndex] + "(context)");
+		}
+	});
+}
+
+// A .harmony file is a zip holding:
+//   image.png     - the raw canvas drawing
+//   map.json      - the current brush's point map { points, count }
+//   manifest.json - { date, selected_brush, canvas: { width, height, pixelRatio } }
+
+function onMenuDownload()
+{
+	canvas.toBlob(function( blob )
+	{
+		blob.arrayBuffer().then(function( pngBuffer )
+		{
+			var brushName = BRUSHES[menu.selector.selectedIndex];
+
+			var manifest = {
+				app: 'harmony',
+				version: 1,
+				date: new Date().toISOString(),
+				selected_brush: brushName,
+				canvas: { width: SCREEN_WIDTH, height: SCREEN_HEIGHT, pixelRatio: PIXEL_RATIO }
+			};
+
+			var map = {
+				points: brush.points || [],
+				count: (typeof brush.count === 'number') ? brush.count : (brush.points ? brush.points.length : 0)
+			};
+
+			var files = {
+				'image.png': new Uint8Array(pngBuffer),
+				'map.json': fflate.strToU8(JSON.stringify(map)),
+				'manifest.json': fflate.strToU8(JSON.stringify(manifest, null, 2))
+			};
+
+			var zipped = fflate.zipSync(files);
+			var url = URL.createObjectURL(new Blob([zipped], { type: 'application/zip' }));
+			var link = document.createElement('a');
+			link.download = 'drawing.harmony';
+			link.href = url;
+			document.body.appendChild(link);
+			link.click();
+			document.body.removeChild(link);
+			URL.revokeObjectURL(url);
+		});
+	}, 'image/png');
+}
+
+function onMenuUpload()
+{
+	menu.fileInput.value = '';
+	menu.fileInput.click();
+}
+
+function onUploadFileChange()
+{
+	var file = menu.fileInput.files[0];
+
+	if (!file)
 		return;
 
-	context.clearRect(0, 0, SCREEN_WIDTH * PIXEL_RATIO, SCREEN_HEIGHT * PIXEL_RATIO);
+	var MAX_FILE_BYTES = 64 * 1024 * 1024;   // reject implausibly large .harmony files up-front
+	var MAX_MAP_BYTES = 16 * 1024 * 1024;    // guard JSON.parse of the point map
+	var MAX_DIM = 8192, MAX_PIXELS = 40000000;
 
-	saveToLocalStorage();
+	if (file.size > MAX_FILE_BYTES)
+	{
+		openModal({ title: 'File too large', message: 'This .harmony file is too large to open.', confirmLabel: 'OK' });
+		return;
+	}
+
+	file.arrayBuffer().then(function( buffer )
+	{
+		var files;
+
+		try {
+			files = fflate.unzipSync(new Uint8Array(buffer));
+		} catch (e) {
+			throw new Error('This is not a valid .harmony file.');
+		}
+
+		if (!files['image.png'] || !files['manifest.json'])
+			throw new Error('This .harmony file is missing required contents.');
+
+		if (files['map.json'] && files['map.json'].byteLength > MAX_MAP_BYTES)
+			throw new Error('The point map in this file is too large.');
+
+		var manifest = JSON.parse(fflate.strFromU8(files['manifest.json']));
+		var rawMap = files['map.json'] ? JSON.parse(fflate.strFromU8(files['map.json'])) : { points: [], count: 0 };
+		var pngBytes = files['image.png'];
+
+		// Safety: read the PNG dimensions from the IHDR header before decoding,
+		// so a decompression bomb is rejected without ever being decoded.
+		var dims = pngDimensions(pngBytes);
+
+		if (dims.width > MAX_DIM || dims.height > MAX_DIM || dims.width * dims.height > MAX_PIXELS)
+			throw new Error('The image in this file is too large to open.');
+
+		// The point map must be coordinated in size with the image.
+		var savedCanvas = manifest.canvas || {};
+		var expectedWidth = Math.round((savedCanvas.width || 0) * (savedCanvas.pixelRatio || 1));
+		var expectedHeight = Math.round((savedCanvas.height || 0) * (savedCanvas.pixelRatio || 1));
+
+		if (expectedWidth !== dims.width || expectedHeight !== dims.height)
+			throw new Error("This file is inconsistent - the image and point map sizes don't match.");
+
+		// Validate the points themselves against the saved canvas coordinate space.
+		var map = validatePointMap(rawMap, savedCanvas.width, savedCanvas.height);
+
+		var apply = function() { applyHarmony(manifest, map, pngBytes); };
+
+		if (isCanvasDirty)
+		{
+			openModal({
+				danger: true,
+				icon: ICON.warning,
+				title: 'Overwrite drawing?',
+				message: "Opening this file will replace your current drawing. This can't be undone.",
+				confirmLabel: 'Open',
+				cancelLabel: 'Cancel',
+				onConfirm: apply
+			});
+		}
+		else
+		{
+			apply();
+		}
+	}).catch(function( error )
+	{
+		openModal({
+			title: 'Could not open file',
+			message: (error && error.message) || 'The file could not be opened.',
+			confirmLabel: 'OK'
+		});
+	});
+}
+
+function validatePointMap( map, width, height )
+{
+	var MAX_POINTS = 100000;   // caps the brushes' O(n^2) neighbour loop
+
+	if (!map || typeof map !== 'object')
+		throw new Error('The point map is malformed.');
+
+	var points = map.points;
+
+	if (points === undefined || points === null)
+		return { points: [], count: 0 };
+
+	if (!Array.isArray(points))
+		throw new Error('The point map is malformed.');
+
+	if (points.length > MAX_POINTS)
+		throw new Error('The point map is too large (' + points.length + ' points).');
+
+	// Points live in the saved canvas coordinate space. Allow a full-canvas
+	// margin on each side so strokes that ran past the edge still pass, while
+	// still rejecting coordinates that clearly don't belong to this image.
+	var minX = -width, maxX = width * 2, minY = -height, maxY = height * 2;
+
+	for (var i = 0; i < points.length; i++)
+	{
+		var p = points[i];
+
+		if (!Array.isArray(p) || p.length < 2 ||
+		    typeof p[0] !== 'number' || typeof p[1] !== 'number' ||
+		    !isFinite(p[0]) || !isFinite(p[1]) ||
+		    p[0] < minX || p[0] > maxX || p[1] < minY || p[1] > maxY)
+			throw new Error('The point map contains coordinates that do not match the image.');
+	}
+
+	var count = (typeof map.count === 'number' && isFinite(map.count)) ? map.count : points.length;
+	if (count < 0 || count > points.length) count = points.length;
+
+	return { points: points, count: count };
+}
+
+function pngDimensions( bytes )
+{
+	var signature = [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
+
+	for (var i = 0; i < 8; i++)
+		if (bytes[i] !== signature[i])
+			throw new Error('The file does not contain a valid PNG image.');
+
+	var view = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
+
+	// Width and height live in the IHDR chunk at byte offsets 16 and 20.
+	return { width: view.getUint32(16), height: view.getUint32(20) };
+}
+
+function applyHarmony( manifest, map, pngBytes )
+{
+	// Select the brush that was active when the file was saved.
+	var index = BRUSHES.indexOf(manifest.selected_brush);
+	if (index === -1) index = menu.selector.selectedIndex;
+
+	menu.selector.selectedIndex = index;
 
 	brush.destroy();
-	brush = eval("new " + BRUSHES[menu.selector.selectedIndex] + "(context)");
+	brush = eval("new " + BRUSHES[index] + "(context)");
+
+	// Inject the saved point map so the brush keeps connecting to it.
+	if (map && map.points) brush.points = map.points;
+	if (map && typeof map.count === 'number') brush.count = map.count;
+
+	window.location.hash = BRUSHES[index];
+
+	// Draw the image at 1:1 device pixels (matching how the canvas was saved).
+	var url = URL.createObjectURL(new Blob([pngBytes], { type: 'image/png' }));
+	var image = new Image();
+
+	image.addEventListener('load', function()
+	{
+		URL.revokeObjectURL(url);
+
+		context.setTransform(1, 0, 0, 1, 0, 0);
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.drawImage(image, 0, 0);
+		context.setTransform(PIXEL_RATIO, 0, 0, PIXEL_RATIO, 0, 0);
+
+		isCanvasDirty = true;
+
+		if (STORAGE) saveToLocalStorage();
+	}, false);
+
+	image.addEventListener('error', function()
+	{
+		URL.revokeObjectURL(url);
+		openModal({ title: 'Could not open image', message: 'The image could not be loaded.', confirmLabel: 'OK' });
+	}, false);
+
+	image.src = url;
+}
+
+function openModal( options )
+{
+	var backdrop = document.createElement('div');
+	backdrop.className = 'modal-backdrop';
+
+	var card = document.createElement('div');
+	card.className = 'modal-card' + (options.danger ? ' danger' : '');
+
+	var markup = '';
+	if (options.icon) markup += '<span class="modal-icon">' + options.icon + '</span>';
+	markup += '<h2>' + options.title + '</h2>';
+	markup += '<p>' + options.message + '</p>';
+	card.innerHTML = markup;
+
+	var actions = document.createElement('div');
+	actions.className = 'modal-actions';
+
+	function close()
+	{
+		isModalOpen = false;
+		document.removeEventListener('keydown', onKey, true);
+		if (backdrop.parentNode) backdrop.parentNode.removeChild(backdrop);
+	}
+
+	function confirm()
+	{
+		close();
+		if (options.onConfirm) options.onConfirm();
+	}
+
+	if (options.cancelLabel)
+	{
+		var cancelButton = document.createElement('button');
+		cancelButton.type = 'button';
+		cancelButton.className = 'modal-btn';
+		cancelButton.textContent = options.cancelLabel;
+		cancelButton.addEventListener('click', close, false);
+		actions.appendChild(cancelButton);
+	}
+
+	var confirmButton = document.createElement('button');
+	confirmButton.type = 'button';
+	confirmButton.className = 'modal-btn primary';
+	confirmButton.textContent = options.confirmLabel || 'OK';
+	confirmButton.addEventListener('click', confirm, false);
+	actions.appendChild(confirmButton);
+
+	card.appendChild(actions);
+	backdrop.appendChild(card);
+
+	backdrop.addEventListener('pointerdown', function( event )
+	{
+		if (event.target === backdrop) close();
+	}, false);
+
+	// Keep focus inside the dialog and default it to the safe choice (Cancel),
+	// so a stray Enter can never trigger the destructive action - the user has
+	// to deliberately move to the confirm button (or click it).
+	var focusable = options.cancelLabel ? [cancelButton, confirmButton] : [confirmButton];
+
+	function onKey( event )
+	{
+		if (event.keyCode === 27)            // Escape cancels
+		{
+			event.stopPropagation();
+			event.preventDefault();
+			close();
+		}
+		else if (event.keyCode === 9)        // Tab cycles between the buttons only
+		{
+			event.preventDefault();
+			var current = focusable.indexOf(document.activeElement);
+			var step = event.shiftKey ? -1 : 1;
+			focusable[(current + step + focusable.length) % focusable.length].focus();
+		}
+		// Enter / Space activate whichever button has focus (Cancel by default).
+	}
+	document.addEventListener('keydown', onKey, true);
+
+	isModalOpen = true;
+	document.body.appendChild(backdrop);
+	focusable[0].focus();
 }
 
 function onMenuAbout()
@@ -444,6 +798,8 @@ function onCanvasMouseDown( event )
 	}
 
 	BRUSH_PRESSURE = wacom && wacom.isWacom ? wacom.pressure : 1;
+
+	isCanvasDirty = true;
 
 	brush.strokeStart( event.clientX, event.clientY );
 
@@ -482,6 +838,8 @@ function onCanvasTouchStart( event )
 	if(event.touches.length == 1)
 	{
 		event.preventDefault();
+
+		isCanvasDirty = true;
 
 		brush.strokeStart( event.touches[0].pageX, event.touches[0].pageY );
 
@@ -547,4 +905,35 @@ function cleanPopUps()
 		about.hide();
 		isAboutVisible = false;
 	}
+}
+
+
+// THEME
+
+function themeGet()
+{
+	try { return localStorage['harmony-theme'] || 'auto'; }
+	catch (e) { return 'auto'; }
+}
+
+function themeSet( value )
+{
+	try { localStorage['harmony-theme'] = value; } catch (e) {}
+	themeApply( value );
+}
+
+function themeApply( value )
+{
+	var prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+	var dark = value === 'dark' || (value === 'auto' && prefersDark);
+
+	document.documentElement.classList.toggle('theme-dark', dark);
+}
+
+if (window.matchMedia)
+{
+	window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function()
+	{
+		if (themeGet() === 'auto') themeApply('auto');
+	});
 }
